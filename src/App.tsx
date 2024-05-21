37bfb9546a253c58ea69d10708e2307691bb68ff
しょうご <155814857+Shogo-Kai@users.noreply.github.com>
2024-05-22 01:22:56 +0900

    一部機能の削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
 import Home from './pages/Home';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <Home />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          いいね！ {count}
-        </button>
-      </div>
     </div>
   );
 }
